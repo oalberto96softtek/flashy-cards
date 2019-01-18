4a2d071fdac6d70d3c533246b0b56c25aeb26a7f
oalberto96 <ochoapublic@gmail.com>
2019-01-17 19:11:24 -0800

    change APP_ENV value

--- django-scripts/run.sh
+++ django-scripts/run.sh
@@ -7,7 +7,7 @@ APP_PATH=${APP_PATH}
 python $APP_PATH/src/./manage.py migrate
 python $APP_PATH/src/./manage.py loaddata seeder
 
-if [ $APP_ENV = "PRODUCTION" ] ; then
+if [ $APP_ENV = "production" ] ; then
 	echo Starting production server...
 	exec uwsgi --http :8000 --chdir $APP_PATH/src --module flashy_cards.wsgi --processes 5 --threads 2 --stats 127.0.0.1:9191 
 else
